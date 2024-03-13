e30eb1cbb3adba9be3ec83882462ccf7f7bdf831
Erik <erikpcdev@gmail.com>
2024-03-13 18:31:19 -0300

    4 commit Parte Responsiva 2 + Detalhes

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,12 @@ function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('light')
 }
+
+const menuDiv = document.getElementById('menu-mobile')
+const animationBtn = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animationMenu)
+function animationMenu(){
+    menuDiv.classList.toggle('open')
+    animationBtn.classList.toggle('active')
+}
